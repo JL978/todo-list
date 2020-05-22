80200f68ad9016643440551c4322754d11dace94
Jimmy Lam <JimmyLam978@gmail.com>
2020-05-22 19:01:16 -0400

    More functionality added

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const newItem = document.querySelector('#newInput')
 
 window.addEventListener('submit', (e)=> {
     e.preventDefault()
-    const a = document.createElement('div')
-    a.innerHTML = newItem.value
+    if (newItem.value !== ''){
+        const a = document.createElement('div')
+    a.innerHTML = `${newItem.value}`
+    a.classList.add('itemLabel')
     newItem.value = ''
     document.querySelector('.items').appendChild(a)
+    }
 })
 
 
