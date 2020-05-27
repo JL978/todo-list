25c3cd61dcf70d93aef7dc39a1aa05dc9b02afe1
Jimmy Lam <JimmyLam978@gmail.com>
2020-05-27 11:54:58 -0400

    fixed crossing out bug and added local storage ability

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const newItem = document.querySelector('#newInput')
+const newInput = document.querySelector('#newInput')
+const items = document.querySelector('.items')
+let itemsList = localStorage.getItem('items') || [];
 
-let items;
+function onLoad(){
+    if (itemsList.length != 0){
+        itemsList = itemsList.split(',')
+        items.innerHTML = itemsList.join('')
+
+        const loadedItems = document.querySelectorAll('.itemLabel')
+        loadedItems.forEach(item => {
+            item.addEventListener('click', handleClick)
+        })
+    }
+}
+
+onLoad()
 
 window.addEventListener('submit', (e)=> {
     e.preventDefault()
-    if (newItem.value !== ''){
+    if (newInput.value !== ''){
         const a = document.createElement('div')
-    a.innerHTML = `${newItem.value}`
+    a.innerHTML = `${newInput.value}`
     a.classList.add('itemLabel')
-    newItem.value = ''
-    document.querySelector('.items').appendChild(a)
+    itemsList.push(a.outerHTML)
+    localStorage.setItem('items', itemsList)
+    newInput.value = ''
+    items.appendChild(a)
     a.addEventListener('click', handleClick)
     }
 })
 
 function handleClick() {
+    let currentIndex = itemsList.indexOf(this.outerHTML)
     this.classList.toggle('crossed')
+    const modified = this.outerHTML
+    itemsList.splice(currentIndex, 1, modified)
+    console.log(itemsList)
+    localStorage.setItem('items', itemsList)
 }
 
 
