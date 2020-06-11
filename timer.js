2f5f9855c83275b671ec3e93853aa15e4c3abbb2
Jimmy Lam <JimmyLam978@gmail.com>
2020-06-11 14:28:39 -0400

    Redid layout to grid, fixed progress movement to ms, added control and input buttons

--- timer.js
+++ timer.js
@@ -6,31 +6,40 @@ setTime(0)
 function timer(seconds){
     const start = Date.now()
     const end = start + seconds*1000
-    const totalTime = (end - start)/1000
-
+    const totalTime = end - start
+    
     setTime(seconds)
-    counter = setInterval(() => {
-        timeLeft = Math.round((end - Date.now())/1000)
-        if (timeLeft < 0){
-            clearInterval(counter)
+    mover = setInterval(()=>{
+        milLeft = end - Date.now()
+        if (milLeft < 0) {
+            clearInterval(mover)
             progress.style.transform = `rotate(0deg)`
             return
         }
-        setTime(timeLeft)
-        updateProgress(timeLeft, totalTime)
+        updateProgress(milLeft, totalTime)
+    })
+
+    counter = setInterval(() => {
+        secLeft = Math.round((end - Date.now())/1000)
+
+        if (secLeft < 0){
+            clearInterval(counter)
+            return
+        }
+        setTime(secLeft)
+        
     }, 1000);
+    
 }
-timer(10)
+timer(0)
 function setTime(seconds){
     const min = Math.floor(seconds/60)
     const sec = seconds%60 
 
-    timerFace.textContent = `${min}:${sec< 10? "0":''}${sec}`
+    timerFace.textContent = `${min< 10? "0":''}${min}:${sec< 10? "0":''}${sec}`
 }
 
 function updateProgress(timeLeft, totalTime){
     degree = (1- timeLeft/totalTime) * 360
-    console.log(degree);
-    
     progress.style.transform = `rotate(${degree}deg)`
 }
