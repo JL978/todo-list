e07a7ec81e75e3585142913439175c47581fccf4
Jimmy Lam <JimmyLam978@gmail.com>
2020-06-12 13:18:24 -0400

    added manual input, restart button is functional

--- timer.js
+++ timer.js
@@ -6,16 +6,22 @@ let counter
 
 setTime(0)
 
-function timer(seconds){
-     clearInterval(mover)
-     clearInterval(counter)
+function timer(seconds=0, resume=false, mil=0){
+    clearInterval(mover)
+    clearInterval(counter)
 
     const start = Date.now()
-    const end = start + seconds*1000
-    const totalTime = end - start
+    if (resume){
+        var end = start + mil
+        var totalTime = runTime*1000
+    }else{
+        var end = start + seconds*1000
+        var totalTime = end - start
+    }
     
     mover = setInterval(()=>{
         milLeft = end - Date.now()
+        localStorage.setItem('mil', milLeft)
         if (milLeft < 0) {
             clearInterval(mover)
             progress.style.transform = `rotate(0deg)`
@@ -49,26 +55,27 @@ function updateProgress(timeLeft, totalTime){
 }
 
 const buttonInputs = document.querySelectorAll(".setTime")
-const manualInput = document.querySelector('.time-input')
+const manualInputDiv = document.querySelector('.time-input')
 const buttons = document.querySelector('.buttons')
 let startButton = document.querySelector("#start")
 const pauseButton = document.querySelector("#pause")
-const resetButton = document.querySelector("#reset")
-
+const resetButton = document.querySelector("#restart")
+const manualInput = document.querySelector('#SetTimer')
 
 buttonInputs.forEach(button => button.addEventListener('click', (e) => {
     runTime = e.target.dataset.min * 60
     setTime(runTime)
+    localStorage.setItem('totalTime', runTime)
 }))
 
 
 function startClick(){
     startButton = document.querySelector("#start")
     if (!startButton.classList.contains("inProgress") && runTime != 0 && runTime){
-        startButton.classList.toggle("inProgress")
+        startButton.classList.add("inProgress")
 
-        manualInput.classList.toggle('hide')
-        buttons.classList.toggle('hide')
+        manualInputDiv.classList.add('hide')
+        buttons.classList.add('hide')
 
         startButton.removeEventListener('click', startClick)
         timer(runTime)
@@ -80,7 +87,62 @@ startButton.addEventListener('click', startClick)
 pauseButton.addEventListener('click', () =>{
     startButton = document.querySelector("#start")
     if (startButton.classList.contains("inProgress")){
-        startButton.classList.toggle("inProgress")
+        clearInterval(mover)
+        clearInterval(counter)
+        startButton.addEventListener('click', startClick)
+        console.log('hi')
+    }
+})
+
+function checkValid(input){
+    if (!isNaN(input)){
+        return true
+    }
+    splited = input.split(':')
+    if (splited.length === 2 &&  0 < splited[0].length <= 2 && splited[1].length === 2){
+        return true
+    }
+    return false
+}
+
+function convertSeconds(input){
+    if (!isNaN(input)){
+        totalSec = input * 60
+    }else{
+        splited = input.split(':')
+        min = splited[0]
+        sec = splited[1]
+        if ((min[0]) === 0){
+            min = min[1]
+        }
+        totalSec = parseInt(min*60) + parseInt(sec)
+    }
+    return totalSec < 3600 ? totalSec:3600
+}
+
+manualInput.addEventListener('keydown', (e)=>{
+    if (isNaN(e.key) && e.key!=='Backspace' && e.key !== ":"){
+        e.preventDefault()
+    }
+    if (e.key === "Enter"){
+        if (checkValid(manualInput.value)){
+            runTime = convertSeconds((manualInput.value))
+            manualInput.value = ''
+            setTime(runTime)
+            localStorage.setItem('totalTime', runTime)
+        }
+    }
+})
+
+resetButton.addEventListener('click', () => {
+    if (startButton.classList.contains("inProgress")){
+        clearInterval(mover)
+        clearInterval(counter)
+        setTime(runTime)
+        progress.style.transform = `rotate(0deg)`
+        manualInputDiv.classList.remove('hide')
+        buttons.classList.remove('hide')
+        startButton.classList.remove("inProgress")
         startButton.addEventListener('click', startClick)
     }
 })
